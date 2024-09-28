eb3095cc7a40412dea6c232a798b43ac9da3f253
Logan Pearson <1800loganp@gmail.com>
2024-09-27 21:39:03 -0400

    Create [0262] Trips and Users (Hard).sql

---
+++ [0262] Trips and Users (Hard).sql
@@ -0,0 +1,139 @@
+"""
+overview: In this SQL query, I calculate the cancellation rate of taxi trips with unbanned users 
+between October 1st and October 3rd, 2013. I first create a CTE that joins the `Trips` and 
+`Users` tables to exclude trips made by banned drivers or clients. It then counts the number of 
+completed trips and total trips for each day using a window function. The final result 
+calculates the daily cancellation rate by subtracting the non-cancelled rate from 1, and the 
+results are rounded to two decimal places.
+"""
+    
+with joined_cte as (
+-- This CTE joins the tables to figure out who's banned and also filters out irrelevent
+    select 
+        status,  
+        request_at
+
+    from Trips t 
+    -- u1 connects the driver_id to it's `banned` status 
+    left join Users u1 on t.driver_id = u1.users_id
+    -- u2 connects the client_id to it's `banned` status  
+    left join Users u2 on t.client_id = u2.users_id
+
+    where request_at between "2013-10-01" and "2013-10-03"
+    and u1.banned = "No"  -- checks that driver is not banned
+    and u2.banned = "No"  -- checks that client is not banned
+
+), counts_cte as(   
+-- This table partitions the joined & filtered data from joined_cte to get the count of completed records on each day along with the total records on each day
+    select
+        distinct request_at,
+        count(case when status = "completed" then 1 end) over(partition by request_at) as completed_count,
+        count(*) over(partition by request_at) as total
+
+    from joined_cte
+)
+-- This main query calculates the cancellation rate by getting the `non-cancelled rate` first (because that number was easier to calculate in the previous cte) then it does 1 minus that number
+select
+    request_at as Day,
+    round(1 - (completed_count / total), 2) as "Cancellation Rate"
+from counts_cte
+
+/* DIRECTIONS 
+
+Table: Trips
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| id          | int      |
+| client_id   | int      |
+| driver_id   | int      |
+| city_id     | int      |
+| status      | enum     |
+| request_at  | varchar  |     
++-------------+----------+
+id is the primary key (column with unique values) for this table.
+The table holds all taxi trips. Each trip has a unique id, while client_id and driver_id are foreign keys to the users_id at the Users table.
+Status is an ENUM (category) type of ('completed', 'cancelled_by_driver', 'cancelled_by_client').
+ 
+
+Table: Users
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| users_id    | int      |
+| banned      | enum     |
+| role        | enum     |
++-------------+----------+
+users_id is the primary key (column with unique values) for this table.
+The table holds all users. Each user has a unique users_id, and role is an ENUM type of ('client', 'driver', 'partner').
+banned is an ENUM (category) type of ('Yes', 'No').
+ 
+
+The cancellation rate is computed by dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
+
+Write a solution to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03". Round Cancellation Rate to two decimal points.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Trips table:
++----+-----------+-----------+---------+---------------------+------------+
+| id | client_id | driver_id | city_id | status              | request_at |
++----+-----------+-----------+---------+---------------------+------------+
+| 1  | 1         | 10        | 1       | completed           | 2013-10-01 |
+| 2  | 2         | 11        | 1       | cancelled_by_driver | 2013-10-01 |
+| 3  | 3         | 12        | 6       | completed           | 2013-10-01 |
+| 4  | 4         | 13        | 6       | cancelled_by_client | 2013-10-01 |
+| 5  | 1         | 10        | 1       | completed           | 2013-10-02 |
+| 6  | 2         | 11        | 6       | completed           | 2013-10-02 |
+| 7  | 3         | 12        | 6       | completed           | 2013-10-02 |
+| 8  | 2         | 12        | 12      | completed           | 2013-10-03 |
+| 9  | 3         | 10        | 12      | completed           | 2013-10-03 |
+| 10 | 4         | 13        | 12      | cancelled_by_driver | 2013-10-03 |
++----+-----------+-----------+---------+---------------------+------------+
+Users table:
++----------+--------+--------+
+| users_id | banned | role   |
++----------+--------+--------+
+| 1        | No     | client |
+| 2        | Yes    | client |
+| 3        | No     | client |
+| 4        | No     | client |
+| 10       | No     | driver |
+| 11       | No     | driver |
+| 12       | No     | driver |
+| 13       | No     | driver |
++----------+--------+--------+
+Output: 
++------------+-------------------+
+| Day        | Cancellation Rate |
++------------+-------------------+
+| 2013-10-01 | 0.33              |
+| 2013-10-02 | 0.00              |
+| 2013-10-03 | 0.50              |
++------------+-------------------+
+Explanation: 
+On 2013-10-01:
+  - There were 4 requests in total, 2 of which were canceled.
+  - However, the request with Id=2 was made by a banned client (User_Id=2), so it is ignored in the calculation.
+  - Hence there are 3 unbanned requests in total, 1 of which was canceled.
+  - The Cancellation Rate is (1 / 3) = 0.33
+On 2013-10-02:
+  - There were 3 requests in total, 0 of which were canceled.
+  - The request with Id=6 was made by a banned client, so it is ignored.
+  - Hence there are 2 unbanned requests in total, 0 of which were canceled.
+  - The Cancellation Rate is (0 / 2) = 0.00
+On 2013-10-03:
+  - There were 3 requests in total, 1 of which was canceled.
+  - The request with Id=8 was made by a banned client, so it is ignored.
+  - Hence there are 2 unbanned request in total, 1 of which were canceled.
+  - The Cancellation Rate is (1 / 2) = 0.50
+*/
