70a4aea0210fd170aa55fd816bed31e2d7cdc413
Logan Pearson <1800loganp@gmail.com>
2025-05-07 21:00:21 -0400

    Update 001_Contains_Duplicate.py

--- python/001_Contains_Duplicate.py
+++ python/001_Contains_Duplicate.py
@@ -1,5 +1,5 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         
-        # if quantity of nums != quantity of unique nums
+        # if quantity of nums != quantity of unique nums then you know there must be duplicate values
         return len(nums) != len(set(nums))
