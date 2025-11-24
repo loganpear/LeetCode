a2cd9775b15e63b5fc8196fbcd2c64661e095317
Logan Pearson <1800loganp@gmail.com>
2025-11-24 15:04:59 -0500

    Implement reachNumber function to solve target problem

---
+++ python/0754_Reach_a_Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        total = 0
+        incriment = 0
+        diff = 0 # initial val doesnt really matter
+
+        # iterate until total is greater than the target
+        # if the diff is even it means as long as our total is >= target there is a possible combo of plus and minus to get there at the current incriment 
+        while total < target or diff % 2 == 1:
+            incriment += 1
+            total += incriment
+            diff = total - target
+            
+        return incriment
